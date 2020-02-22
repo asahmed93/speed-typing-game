ce968156f6a449c6d875fccd21d8802a86f7839b
Lambda_School_Loaner_264 <lambda_school_loaner_264@Lambda-School-Loaner-264s-MacBook-Pro.local>
2020-02-22 01:29:49 -0500

    Added more logic to make app more reactive

--- speed-typer/src/App.js
+++ speed-typer/src/App.js
@@ -1,12 +1,27 @@
-import React from "react"
+import React, {useState} from "react"
 
 function App() {
+    const [text, setText] = useState("")
+    
+    function handleChange(e) {
+        const {value} = e.target
+        setText(value)
+    }
+    
+    function calculateWordCount(text) {
+        const wordsArr = text.trim().split(" ")
+        return wordsArr.filter(word => word !== "").length
+    }
+    
     return (
         <div>
             <h1>How fast do you type?</h1>
-            <textarea />
-            <h4>Time reminaing: ???</h4>
-            <button>Start</button>
+            <textarea
+                onChange={handleChange}
+                value={text}
+            />
+            <h4>Time remaining: ???</h4>
+            <button onClick={() => console.log(calculateWordCount(text))}>Start</button>
             <h1>Word count: ???</h1>
         </div>
     )
