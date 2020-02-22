8b3bd56faeedf97644f3dba34286d39485743e22
Lambda_School_Loaner_264 <lambda_school_loaner_264@Lambda-School-Loaner-264s-MacBook-Pro.local>
2020-02-22 01:46:32 -0500

    increased timer

--- speed-typer/src/App.js
+++ speed-typer/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect, useRef} from "react"
 
 function App() {
-    const STARTING_TIME = 5
+    const STARTING_TIME = 60
     
     const [text, setText] = useState("")
     const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
